3e292fad78ad5b9aa586c25b7946323710d003ca
EaryOrtiz <eary117@hotmail.com>
2019-03-07 17:29:52 -0600

    Slowdown Table Complete

--- ProdFloor/Models/EnginnerRefTables.cs
+++ ProdFloor/Models/EnginnerRefTables.cs
@@ -10,18 +10,22 @@ namespace ProdFloor.Models
     {
         public int SlowdownID { get; set; }
 
+        [Display(Name ="Car Speed (FPM)")]
         [Required(ErrorMessage = "Please enter a Car Speed")]
         public int CarSpeedFPM { get; set; }
 
         [Required(ErrorMessage = "Please enter a Distance")]
         public int Distance { get; set; }
 
+        [Display(Name = "Landing Page (A)")]
         [Required(ErrorMessage = "Please enter an A")]
         public int A { get; set; }
 
+        [Display(Name = "Slow Limit")]
         [Required(ErrorMessage = "Please enter a Slow Limit")]
         public int SlowLimit { get; set; }
 
+        [Display(Name = "Minium Floor Heigth")]
         [Required(ErrorMessage = "Please enter a Minium Floor Height")]
         public int MiniumFloorHeight { get; set; }
     }
@@ -36,6 +40,7 @@ namespace ProdFloor.Models
         [Required(ErrorMessage = "Please enter an Size")]
         public string Size { get; set; }
 
+        [Display(Name = "AMP Rating")]
         [Required(ErrorMessage = "Please enter an AMP Rating")]
         public int AMPRating { get; set; }
     }
@@ -59,12 +64,15 @@ namespace ProdFloor.Models
         [Required(ErrorMessage = "Please enter a HP Number")]
         public float HP { get; set; }
 
+        [Display(Name = "MCE Part Number")]
         [Required(ErrorMessage = "Please enter a MCE Part Number")]
         public string MCPart { get; set; }
 
-        [Required(ErrorMessage = "Please enter a New Manufacturer Part")]
+        [Display(Name = "New Manufacturer Part Number")]
+        [Required(ErrorMessage = "Please enter a New Manufacturer Part Number")]
         public string NewManufacturerPart { get; set; }
 
+        [Display(Name = "Overload Table Number")]
         [Required(ErrorMessage = "Please enter a Overload Table Number")]
         public string OverloadTable { get; set; }
     }
@@ -73,18 +81,23 @@ namespace ProdFloor.Models
     {
         public int OverloadID { get; set; }
 
+        [Display(Name = "AMP Minium")]
         [Required(ErrorMessage = "Please enter a AMP Min")]
         public float AMPMin { get; set; }
 
+        [Display(Name = "AMP Maximum")]
         [Required(ErrorMessage = "Please enter a AMP Max")]
         public float AMPMax { get; set; }
 
+        [Display(Name = "Overload Table Number")]
         [Required(ErrorMessage = "Please enter a Overload Table Number")]
         public int OverTableNum { get; set; }
 
+        [Display(Name = "MCE Part Number")]
         [Required(ErrorMessage = "Please enter a MCE Part Number")]
         public string MCPart { get; set; }
 
+        [Display(Name = "Siemens Part Number")]
         [Required(ErrorMessage = "Please enter a Siemens Part")]
         public string SiemensPart { get; set; }
     }
