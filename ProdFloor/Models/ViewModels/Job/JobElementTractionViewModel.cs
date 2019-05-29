48a145933beafeec41d11b6152756a52631b5b54
EaryOrtiz <eary117@hotmail.com>
2019-05-29 11:33:08 -0500

    New view Models added

---
+++ ProdFloor/Models/ViewModels/Job/JobElementTractionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdFloor.Models.ViewModels.Job
+{
+    public class JobElementTractionViewModel
+    {
+        public Models.Job CurrentJob { get; set; }
+        public Element Element { get; set; }
+        public ElementTraction ElementTraction { get; set; }
+        public List<SpecialFeatures> SpecialFeatureslist { get; set; }
+        public string CurrentTab { get; set; }
+        public string buttonAction { get; set; }
+        public int CurrentUserID { get; set; }
+        public int fieldID { get; set; }
+    }
+}
