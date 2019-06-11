5a6a3c1af680474336b5c8b515d76b9f98636cd8
EaryOrtiz <46458268+EaryOrtiz@users.noreply.github.com>
2019-06-11 11:55:17 -0500

    asd

--- ProdFloor/Startup.cs
+++ ProdFloor/Startup.cs
@@ -131,7 +131,7 @@ namespace ProdFloor
                  name: null,
                     template: "{controller=Job}/{action=JobSearchList}/{id?}");
             });
-            SeedData.EnsurePopulated(app);
+            //SeedData.EnsurePopulated(app);
             //IdentitySeedData.EnsurePopulated(app);
         }
     }
