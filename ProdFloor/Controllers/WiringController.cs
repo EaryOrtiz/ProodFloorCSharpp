9d2c2879a4c7997720b1b207300da57fbb9efb94
EaryOrtiz <eary117@hotmail.com>
2021-04-12 16:39:04 -0500

    body for wirer added

---
+++ ProdFloor/Controllers/WiringController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProdFloor.Models;
+
+namespace ProdFloor.Controllers
+{
+    public class WiringController : Controller
+    {
+        private IJobRepository jobRepo;
+        private IWiringRepository wiringRepo;
+        private UserManager<AppUser> userManager;
+        private IHostingEnvironment _env;
+        public int PageSize = 7;
+        string appDataFolder => _env.WebRootPath.ToString() + @"\AppData\";
+
+        public WiringController(IWiringRepository repo,
+            IJobRepository repo2,
+            UserManager<AppUser> userMgr,
+            IHostingEnvironment env)
+        {
+            jobRepo = repo2;
+            wiringRepo = repo;
+            userManager = userMgr;
+            _env = env;
+        }
+
+
+
+        public IActionResult ProductionAdminDash()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProdFloor/Models/EFWiringRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdFloor.Models
+{
+    public class EFWiringRepository : IWiringRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFWiringRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+    }
+}
