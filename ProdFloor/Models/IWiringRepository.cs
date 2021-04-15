920b2bdc8bd9058b93b68b6ed7420eae56303799
EaryOrtiz <eary117@hotmail.com>
2021-04-15 16:24:04 -0500

    PXPClasses added and PXP in repositories

--- ProdFloor/Models/IWiringRepository.cs
+++ ProdFloor/Models/IWiringRepository.cs
@@ -7,5 +7,16 @@ namespace ProdFloor.Models
 {
     public interface IWiringRepository
     {
+        IQueryable<WirerPXP> wirerPXPs { get; }
+        IQueryable<PXPError> pXPErrors { get; }
+        IQueryable<PXPReason> pXPReasons { get; }
+
+        void SaveWirerPXP(WirerPXP wirerPXP);
+        void SavePXPError(PXPError pXPError);
+        void SavePXPReason(PXPReason pXPReason);
+
+        TestJob DeleteWirerPXP(int WirerPXPID);
+        TestJob DeletePXPError(int PXPErrorID);
+        TestJob DeletePXPReason(int PXPReasonID);
     }
 }
---
+++ ProdFloor/Models/WirerPXP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdFloor.Models
+{
+    public class WirerPXP
+    {
+        public int WirerPXPID { get; set; }
+
+        public int JobID { get; set; }
+
+        public int SinglePO { get; set; }
+
+        public int WirerID { get; set; }
+
+        public List<PXPError> _PXPErrors { get; set; }
+    }
+
+    public class PXPError
+    {
+        public int PXPErrorID { get; set; }
+
+        public int WirerPXPID { get; set; }
+
+        public int PXPReasonID { get; set; }
+
+        public int GuiltyWirerID { get; set; }
+
+
+    }
+
+    public class PXPReason
+    {
+        public int PXPReasonID { get; set; }
+
+        public int Description { get; set; }
+
+        public List<PXPError> _PXPErrors { get;set;}
+
+    }
+}
