c508931ed72dfe21a046d1960b002942778f93bf
EaryOrtiz <eary117@hotmail.com>
2021-06-29 16:45:30 -0500

    wirirng added to interface and DBContext

--- ProdFloor/Models/IWiringRepository.cs
+++ ProdFloor/Models/IWiringRepository.cs
@@ -7,6 +7,50 @@ namespace ProdFloor.Models
 {
     public interface IWiringRepository
     {
+        //Wiring
+        IQueryable<Wiring> Wirings { get; }
+        IQueryable<WirersInvolved> WirersInvolveds { get; }
+        IQueryable<WiringStop> WiringStops { get; }
+        IQueryable<WiringStep> WiringSteps { get; }
+        IQueryable<WiringStepForJob> WiringStepsForJobs { get; }
+        IQueryable<WiringReason1> WiringReason1s { get; }
+        IQueryable<WiringReason2> WiringReason2s { get; }
+        IQueryable<WiringReason3> WiringReason3s { get; }
+        IQueryable<WiringReason4> WiringReason4s { get; }
+        IQueryable<WiringReason5> WiringReason5s { get; }
+        IQueryable<WiringOption> WiringOptions { get; }
+        IQueryable<WiringFeatures> WiringFeatures { get; }
+        IQueryable<WiringTriggeringFeature> WiringTriggeringFeatures { get; }
+
+        void SaveWiring(Wiring wiring);
+        void SaveWirersInvolved(WirersInvolved wirersInvolved);
+        void SaveWiringStop(WiringStop wiringStop);
+        void SaveWiringStep(WiringStep wiringStep);
+        void SaveWiringStepForJob(WiringStepForJob wiringStepForJob);
+        void SaveWiringReason1(WiringReason1 wiringReason1);
+        void SaveWiringReason2(WiringReason1 wiringReason2);
+        void SaveWiringReason3(WiringReason1 wiringReason3);
+        void SaveWiringReason4(WiringReason1 wiringReason4);
+        void SaveWiringReason5(WiringReason1 wiringReason5);
+        void SaveWiringOption(WiringOption wiringOption);
+        void SaveWiringFeatures(WiringFeatures wiringFeatures);
+        void SaveWiringTriggeringFeature(WiringTriggeringFeature wiringTriggeringFeature);
+
+        Wiring DeleteWiring(int WiringID);
+        WirersInvolved DeleteWirersInvolved(int wirersInvolvedID);
+        WiringStop DeleteWiringStop(int WiringStopID);
+        WiringStep DeleteWiringStep(int wiringStepID);
+        WiringStepForJob DeleteWiringStepForJob(int wiringStepForJobID);
+        WiringReason1 DeleteWiringReason1(int wiringReason1ID);
+        WiringReason2 DeleteWiringReason2(int wiringReason2ID);
+        WiringReason5 DeleteWiringReason3(int wiringReason3ID);
+        WiringReason3 DeleteWiringReason4(int wiringReason4ID);
+        WiringReason5 DeleteWiringReason5(int wiringReason5ID);
+        WiringOption DeleteWiringOption(int wiringOptionID);
+        WiringFeatures DeleteWiringFeatures(int wiringFeaturesID);
+        WiringTriggeringFeature DeleteWiringTriggeringFeature(int wiringTriggeringFeatureID);
+
+        //PXP
         IQueryable<WiringPXP> WiringPXPs { get; }
         IQueryable<PXPError> PXPErrors { get; }
         IQueryable<PXPReason> PXPReasons { get; }
