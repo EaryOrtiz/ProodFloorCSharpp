b5ab5b6185a1f0e91ad23fa8d3939fc0274decf7
EaryOrtiz <eary117@hotmail.com>
2021-07-05 15:34:58 -0500

    WringReasonsJS added

---
+++ ProdFloor/wwwroot/Scripts/CascadingWiringR1R2.js
@@ -0,0 +1,51 @@
+$(function () {
+            $("#WiringReason1ID").change(function () {
+                var Reason1ID = $("#WiringReason1ID").val();
+                var Reason2ID = $('#WiringReason2ID');
+                Reason2ID.empty();
+                if (Reason1ID != null && Reason1ID != '') {
+                    $.ajax({
+                        type: 'GET',
+                        url: '/WiringReasons/GetReason2',
+                        contentType: "applications/json",
+                        data: {
+                            Reason1ID: Reason1ID
+                        },
+                        success: function (data) {
+                            $("#WiringReason2ID").prop("disabled", false);
+                            Reason2ID.append('<option value="">' + " --- Please Select a Reason 2--- " + '</option>');
+                            $.each(data, function (idx, reason) {
+                                Reason2ID.append('<option value="' + reason.value + '">' + reason.text + '</option>');
+                            });
+                        },
+                        error: function (exc) {
+                            alert("error");
+                        }
+                    });
+                }
+            }).ready(function () {
+                var Reason1ID = $("#WiringReason1ID").val();
+                var Reason2ID = $('#WiringReason2ID');
+                Reason2ID.empty();
+                if (Reason1ID != null && Reason1ID != '') {
+                    $.ajax({
+                        type: 'GET',
+                        url: '/WiringReasons/GetReason2',
+                        contentType: "applications/json",
+                        data: {
+                            Reason1ID: Reason1ID
+                        },
+                        success: function (data) {
+                            $("#WiringReason2ID").prop("disabled", false);
+                            Reason2ID.append('<option value="">' + " --- Please Select a Reason 2--- " + '</option>');
+                            $.each(data, function (idx, reason) {
+                                Reason2ID.append('<option value="' + reason.value + '">' + reason.text + '</option>');
+                            });
+                        },
+                        error: function (exc) {
+                            alert("error");
+                        }
+                    });
+                }
+            });
+        });
